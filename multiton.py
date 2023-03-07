56344759173ed6b712c88865180d334f000598d2
AtavazdGevorgyan <ag15012003@gmail.com>
2023-03-07 17:23:02 +0400

    _

--- multiton.py
+++ multiton.py
@@ -2,12 +2,6 @@ class Multiton:
     _counter = 0
     _instances = [None, None, None]
 
-
-
-
-
-
-
     def __new__(cls):
         if not cls._instances[cls._counter % 3]:
             cls._instances[cls._counter % 3] = super().__new__(cls)
